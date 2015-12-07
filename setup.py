9c9895253a0d351de141ea2c7ee43aaf28c658e0
Alejandro M. Bernardis <alejandro.m.bernardis@gmail.com>
2015-12-07 20:13:26 -0300

    FIX // Setup file

--- setup.py
+++ setup.py
@@ -2,15 +2,29 @@ from distutils.core import setup
 from distutils.extension import Extension
 
 if 3/2 == 1.5:
-    version = 3
+    ext_modules = [Extension('frozen_dict', ['frozen_dict_py3.c'])]
 else:
-    version = 2
+    ext_modules = [Extension('frozen_dict', ['frozen_dict_py2.c'])]
 
-if version == 3:
-    setup(
-        ext_modules = [Extension('frozen_dict', ['frozen_dict_py3.c'])]
-    )
-else:
-    setup(
-        ext_modules = [Extension('frozen_dict', ['frozen_dict_py2.c'])]
-    )
+
+with open('README.md', 'r') as f:
+    readme = f.read()
+
+setup(
+    name='frozen-dict',
+    version='last',
+    url='https://github.com/alejandrobernardis/frozen_dict',
+    license='MIT',
+    author='Steve Zelaznik',
+    author_email='zelaznik@users.noreply.github.com',
+    description='Creates a class FrozenDict that behaves like dict, but all '\
+                'mutable methods are removed. - Compatible with both Python2 '\
+                'and Python3. - Like a tuple, FrozenDict is hashable if and '\
+                'only if all its items are hashable.',
+    long_description=readme,
+    package_data={'': ['LICENSE', 'README.md']},
+    ext_modules=ext_modules,
+    include_package_data=True,
+    zip_safe=False,
+    platforms=['Linux']
+)
